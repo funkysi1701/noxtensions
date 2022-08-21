0652ac99b26c374f2973597909963a4e3ce77f61
Simon Foster <simon@funkysi1701.com>
2022-08-21 16:27:28 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("A F E F D G E F ", 120)
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 4; index++) {
+        music.playTone(494, music.beat(BeatFraction.Double))
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    music.playTone(988, music.beat(BeatFraction.Whole))
+    music.playTone(494, music.beat(BeatFraction.Whole))
+    music.playTone(247, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        # # # # #
+        . # # # .
+        . # # # .
+        `)
+    basic.showIcon(IconNames.Skull)
+})
